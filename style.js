ab8139cac92f90add47d11ebe8b8fb5dc9ebcd88
Tomas Lozano <lozanotdlc@gmail.com>
2024-05-27 13:54:22 -0300

    primer commit

---
+++ style.js
@@ -0,0 +1,15 @@
+function irArriba(pxPanttalla){
+    window.addEventListener("scroll", () => {
+        var scroll = document.documentElement.scrollTop;
+        console.log(scroll);
+        var botonarriba = document.getElementById("botonarriba");
+
+        if(scroll > pxPanttalla){
+            botonarriba.style.right = 20 + "px";
+        } else {
+            botonarriba.style.right = -100 + "px";
+        }
+    })
+}
+
+irArriba(700);
